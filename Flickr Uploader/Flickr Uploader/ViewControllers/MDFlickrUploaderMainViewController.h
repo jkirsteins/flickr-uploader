5d1c0843b3277f76865184734a6770d466c4531d
Jānis Kiršteins <janis@montadigital.com>
2013-06-17 07:09:15 +0300

    Added initial photo listing code
    Enumerates and prints the filename
    of every photo in the user's library.

---
+++ Flickr Uploader/Flickr Uploader/ViewControllers/MDFlickrUploaderMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MDFlickrUploaderMainViewController.h
+//  Flickr Uploader
+//
+//  Created by Jānis Kiršteins on 17.06.13.
+//  Copyright (c) 2013. g. SIA MONTA DIGITAL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MDFlickrUploaderMainViewController : UIViewController
+
+@end
